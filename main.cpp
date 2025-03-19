02a1c9ad539943ef1a9e169296b58fa460bb01ce
Julián Stejman <jstejman@fi.uba.ar>
2025-03-19 23:39:25 +0000

    digitalin test

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 
 int main()
 {
+    /*
+    DigitalIn es una clase de MBED que en su constructor recibe una variable pin, la cual es un
+    tipo enumerativo PinName definida por STM 
+      
+    */
     DigitalIn gasDetector(D2);
-
     DigitalOut alarmLed(LED1);
 
     gasDetector.mode(PullDown);
@@ -15,10 +19,12 @@ int main()
     while (true) {
         if ( gasDetector == ON ) {
             alarmLed = ON;
+            printf("1\n");
         }
         
         if ( gasDetector == OFF ) {
             alarmLed = OFF;
+            printf("0\n");
         }
     }
 }
